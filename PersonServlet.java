be11a90016fb279dd05981f550f2469703f90bda
reshwin <support@SharewareDreams.com>
2023-11-06 00:39:52 +0530

    updates

---
+++ PersonServlet.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+public class PersonServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		
+		String requestUrl = request.getRequestURI();
+		String name = requestUrl.substring("/people/".length());
+		
+		Person person = DataStore.getInstance().getPerson(name);
+		
+		if(person != null){
+			String json = "{\n";
+			json += "\"name\": " + JSONObject.quote(person.getName()) + ",\n";
+			json += "\"about\": " + JSONObject.quote(person.getAbout()) + ",\n";
+			json += "\"birthYear\": " + person.getBirthYear() + "\n";
+			json += "}";
+			response.getOutputStream().println(json);
+		}
+		else{
+			//That person wasn't found, so return an empty JSON object. We could also return an error.
+			response.getOutputStream().println("{eugene}");
+		}
+	}
+	
+	
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		String name = request.getParameter("name");
+		String about = request.getParameter("about");
+		int birthYear = Integer.parseInt(request.getParameter("birthYear"));
+		
+		DataStore.getInstance().putPerson(new Person(name, about, birthYear));
+	}
+}
